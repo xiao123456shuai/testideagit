1e3e51916e22c1c183b535d26552e7e48089c29a
肖老师 <3066117006@qq.com>
2020-04-17 11:25:07 +0800

    初始化

---
+++ src/cn/xiao/Hello.java
@@ -0,0 +1,16 @@
+package cn.xiao;
+
+/**
+ * @author：xiao
+ * @date：Created in 2020/4/17 11:23
+ * @description：
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("helloword... ...");
+
+    }
+
+}
